b132d9ae5af1c6e9027917dd7f7ef5c7cfababb2
Zaylon Freeman <freemanzaylon548@gmail.com>
2025-11-19 20:30:57 -0800

    wip: added feature icons section and initial layout for spans

--- Full_Stack_Portfolio/crm-app-react/z-force-crm/src/Pages/Homepage.jsx
+++ Full_Stack_Portfolio/crm-app-react/z-force-crm/src/Pages/Homepage.jsx
@@ -9,6 +9,18 @@ const Homepage = () => {
             <div className="mainpage-handshake">
                 <img src="/Handshake.jpg" alt="handshake" />
                 <h1> Empowering you to win</h1>
+                <div className="mainpages-attributes">
+                    <img src="/Person.svg" alt="brainimage" />
+                    <img src="/Ticket.svg" alt="ticketimage" />
+                    <img src="/Business.svg" alt="businessimage" />
+                    <img src="/Lock.svg" alt="lockimage" />
+                </div>
+                <div className="mainpages-attributes-desc">
+                    <span>AI-powered insights that elevate every decision.</span>
+                    <span>An integrated ticketing system that keeps customers and teams perfectly aligned, ensuring complete visibility</span>
+                    <span>Streamline daily operations with a unified system built for clarity and control.</span>
+                    <span>Protect your data with enterprise-level security built on modern compliance standards.</span>
+                </div>
             </div>
         </>
     )
